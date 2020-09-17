2a1c722ced5c3d6206eced537faac03828627d23
Þórarinn Ingi Tómasson <thorarinnt@tmsoftware.is>
2020-09-17 19:32:46 +0000

    Traverse 3

--- tile_traveller.py
+++ tile_traveller.py
@@ -30,17 +30,17 @@ def print_available_directions(directions:str) -> str:
     print("You can travel: {}".format(available_directions))
 
 def traverse(current_tile:str, direction:str) -> str:
-        # splitta
-        tpart = current_tile.split(',')
-        if direction.lower() == 'n':
-            tpart[1] = str(int(tpart[1]) + 1)
-        if direction.lower() == 'e':
-            tpart[0] = str(int(tpart[0]) + 1)
-        if direction.lower() == 's':
-            tpart[1] = str(int(tpart[1]) - 1)
-        if direction.lower() == 'w':
-            tpart[0] = str(int(tpart[0]) - 1)
-        return tpart[0] + ',' + tpart[1]
+    # splitta
+    tpart = current_tile.split(',')
+    if direction.lower() == 'n':
+        tpart[1] = str(int(tpart[1]) + 1)
+    if direction.lower() == 'e':
+        tpart[0] = str(int(tpart[0]) + 1)
+    if direction.lower() == 's':
+        tpart[1] = str(int(tpart[1]) - 1)
+    if direction.lower() == 'w':
+        tpart[0] = str(int(tpart[0]) - 1)
+    return tpart[0] + ',' + tpart[1]
    
 
 run = True
@@ -52,7 +52,7 @@ while run:
         current_tile = traverse(current_tile, direction)
     else: 
         print("nope")
-        return current_tile
+       
     print(current_tile)
     
     
