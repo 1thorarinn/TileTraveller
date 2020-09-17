c6d7bd13517dfcc1f648dd8ca7969798f8718bb5
Þórarinn Ingi Tómasson <thorarinnt@tmsoftware.is>
2020-09-17 19:20:31 +0000

    Traverse

--- tile_traveller.py
+++ tile_traveller.py
@@ -3,14 +3,15 @@
 # Leikmaður byrjar á 1.1 reitnum og þarf að komast á 3.1
 # Leikmaður hefur um að velja norður, suður, austur og vestur.
 # Valmöguleikarnir eru takmarkaðir eftir því á hvaða svæði hann er
-# Til að mynda á 1.1 getur hann bara farið norður
+# Til að 
+# mynda á 1.1 getur hann bara farið norður
 
 
 def available_directions(current_tile:str) -> str:
      return {
         '1,1': 'n',
         '1,2': 'nes',
-        '1,3': 'nes',
+        '1,3': 'se',
         '1,4': 'nes',
         '1,5': 'nes',
         '1,6': 'nes',
@@ -24,12 +25,32 @@ def print_available_directions(directions:str) -> str:
             's': '(S)outh',
             'e': '(E)ast',
             'w': '(W)est',
-        }[chr]
+        }[chr.lower()]
     print("You can travel: {}".format(available_directions))
+
+def traverse(current_tile:str, direction:str) -> str:
+    if direction in available_directions(current_tile):
+        # splitta
+        tpart = current_tile.split(',')
+        if direction.lower() == 'n':
+            tpart[1] = str(int(tpart[1]) + 1)
+        if direction.lower() == 'e':
+            tpart[0] = str(int(tpart[0]) + 1)
+        if direction.lower() == 's':
+            tpart[1] = str(int(tpart[1]) - 1)
+        if direction.lower() == 'w':
+            tpart[0] = str(int(tpart[0]) - 1)
+        return tpart[0] + ',' + tpart[1]
+    else: 
+        print("nope")
+        return current_tile
+
 run = True
 current_tile = "1,1"
 while run:
-    print(print_available_directions(available_directions(current_tile)))
+    print( print_available_directions( available_directions(current_tile) ) )
     direction = input("Direction: ")
+    current_tile = traverse(current_tile, direction)
+    print(current_tile)
     
     
