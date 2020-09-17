d725e980bd04c55b66312367b910ea52d10ff0c5
Haukur Ingimarsson <haukur@Haukurs-MacBook-Pro.local>
2020-09-17 18:09:40 +0000

    Nýjasta

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,1 +1,6 @@
-# FOrrit sem er leikur
+# Forrit sem er leikur
+
+# Leikmaður byrjar á 1.1 reitnum og þarf að komast á 3.1
+# Leikmaður hefur um að velja norður, suður, austur og vestur.
+# Valmöguleikarnir eru takmarkaðir eftir því á hvaða svæði hann er
+# Til að mynda á 1.1 getur hann bara farið norður
